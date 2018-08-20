94079ff512baaaf078dd2348357a083c5b775e2b
ts0115262 <jiang-hao@hotmail.com>
2018-08-20 21:42:01 +0800

    2018-8-20
    1-5 首页框架—TabLayout逻辑实现
    设置悬浮窗的创建，第一个Fragment隐藏
    SettingActivity建立

--- app/src/main/java/com/example/project/smartbutler/MainActivity.java
+++ app/src/main/java/com/example/project/smartbutler/MainActivity.java
@@ -1,24 +1,32 @@
 package com.example.project.smartbutler;
 
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import com.example.project.smartbutler.fragment.ButlerFragment;
 import com.example.project.smartbutler.fragment.GirlFragment;
 import com.example.project.smartbutler.fragment.UserFragment;
 import com.example.project.smartbutler.fragment.WeChatFragment;
+import com.example.project.smartbutler.ui.SettingActivity;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private final String TAG ="MainActivity";
     //TableLayout
     private TabLayout mTabLayout;
     //ViewPager
@@ -27,6 +35,8 @@ public class MainActivity extends AppCompatActivity {
     private List<String> mTitle;
     //Fragment
     private List<Fragment> mFragment;
+    //悬浮窗
+    private FloatingActionButton fab_setting;
 
 
     @Override
@@ -58,8 +68,35 @@ public class MainActivity extends AppCompatActivity {
     private void initView() {
         mTabLayout = findViewById(R.id.mTabLayout);
         mViewPager = findViewById(R.id.mViewPager);
+        fab_setting=findViewById(R.id.fab_setting);
+        fab_setting.setOnClickListener(this);
+        fab_setting.setVisibility(View.GONE);
+
         //预加载
         mViewPager.setOffscreenPageLimit(mFragment.size());
+
+        //mViewPager滑动监听
+        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                Log.d(TAG, "onPageSelected: "+position);
+                if (position==0){
+                    fab_setting.setVisibility(View.GONE);
+                }else {
+                    fab_setting.setVisibility(View.VISIBLE);
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
 
         //设置适配器
         mViewPager.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager()) {
@@ -84,5 +121,14 @@ public class MainActivity extends AppCompatActivity {
 
         //绑定
         mTabLayout.setupWithViewPager(mViewPager);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.fab_setting:
+                startActivity(new Intent(this, SettingActivity.class));
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/example/project/smartbutler/ui/SettingActivity.java
@@ -0,0 +1,15 @@
+package com.example.project.smartbutler.ui;
+
+import android.os.Bundle;
+
+import com.example.project.smartbutler.R;
+
+
+public class SettingActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+    }
+}
