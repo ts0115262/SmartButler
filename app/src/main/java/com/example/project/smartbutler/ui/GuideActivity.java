4502a96007465b7fbf8f764a110ef7907c9ba3b0
ts0115262 <jiang-hao@hotmail.com>
2018-08-25 11:05:34 +0800

    2018-8-25
    2-5写了一半

--- app/src/main/java/com/example/project/smartbutler/ui/GuideActivity.java
+++ app/src/main/java/com/example/project/smartbutler/ui/GuideActivity.java
@@ -1,15 +1,69 @@
 package com.example.project.smartbutler.ui;
 
+import android.support.annotation.NonNull;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.example.project.smartbutler.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GuideActivity extends AppCompatActivity {
+
+    private ViewPager mViewPager;
+    //容器
+    private List<View> mList = new ArrayList<>();
+    private View view1, view2, view3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guide);
+        initView();
+    }
+
+    //初始化View
+    private void initView() {
+        mViewPager = findViewById(R.id.mViewPager);
+
+        view1 = View.inflate(this, R.layout.pager_item_one, null);
+        view2 = View.inflate(this, R.layout.pager_item_two, null);
+        view3 = View.inflate(this, R.layout.pager_item_three, null);
+        mList.add(view1);
+        mList.add(view2);
+        mList.add(view3);
+
+        //设置适配器
+        mViewPager.setAdapter(new GuideAdapter());
+    }
+
+    private class GuideAdapter extends PagerAdapter {
+
+        @Override
+        public int getCount() {
+            return mList.size();
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view == object;
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            ((ViewPager) container).addView(mList.get(position));
+            return mList.get(position);
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            ((ViewPager) container).removeView(mList.get(position));
+        }
     }
 }
